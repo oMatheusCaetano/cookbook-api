cca7ea9c5e7f56fdf4758f1ff4174767e5868721
Matheus Caetano <caetanomatheus876@gmail.com>
2025-09-23 22:33:06 -0300

    feat: login feature tests

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
@@ -14,10 +15,37 @@ class LoginTest extends TestCase
         ]);
     }
 
-    public function test_validates_request_data(): void
+    public function test_should_validate_request_data(): void
     {
         $response = $this->makeRequest();
         $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
         $response->assertJsonValidationErrors(['email', 'password']);
+    }
+
+    public function test_should_return_invalid_credentials_error(): void
+    {
+        Artisan::call('migrate:fresh');
+        $payload = [ 'email' => 'testinvalidemail@testinvalidemail.com', 'password' => 'invalidpassword' ];
+        $response = $this->makeRequest($payload);
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        $response->assertJsonFragment([ 'message' => 'Credenciais inválidas' ]);
+    }
+
+    public function test_should_login_successfully(): void
+    {
+        Artisan::call('migrate:fresh');
+        $payload = [ 'email' => 'usuarioteste@email.com', 'password' => '123456789' ];
+        $response = $this->makeRequest($payload);
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonStructure([
+            'token',
+            'user' => [
+                'id',
+                'name',
+                'email',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
     }
 }
