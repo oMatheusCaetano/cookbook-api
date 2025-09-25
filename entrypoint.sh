4b2b9d32613a2baa320efa32560dad1c5ec30a9d
Matheus Caetano <caetanomatheus876@gmail.com>
2025-09-25 01:31:45 -0300

    fix: docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+echo "===> Installing composer dependencies..."
 composer install
 
+echo "===> Running database migrations..."
 php artisan migrate:fresh --seed
 
+echo "===> Starting Apache server..."
 exec apache2-foreground
